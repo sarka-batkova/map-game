08f7f56055a1b5f163b31d2c8ed8886bd645c3e4
Sarka Batkova <sarka.batkova@gmail.com>
2021-04-30 11:41:58 +0200

    functionality_finished

--- cards.py
+++ cards.py
@@ -12,8 +12,8 @@ def draw_letter(letter, pos_x, pos_y):
     message(letter, gd.red, 100, pos_x + 75, pos_y + 55)
 
 class Card:
-    def __init__(self, image, number):
-        self.image = image
+    def __init__(self, name, number):
+        self.image = pygame.image.load(rf".\karticky\{name}.png")
         self.number = number
 
     def draw_card(self, pos_x, pos_y):
@@ -21,59 +21,67 @@ class Card:
         card = pygame.transform.smoothscale(card, (200, 300)) # new image size
         gd.display.blit(card, (pos_x, pos_y))
 
-Seasons = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\18.png'), 8), 
-            Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\19.png'), 8),
-            Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\20.png'), 7),
-            Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\21.png'), 6)]       
+seasons = [Card('18', 8), 
+           Card('19', 8),
+           Card('20', 7),
+           Card('21', 6)]       
 
+forrest_rules_cards = [Card('26', 0), 
+                       Card('27', 0), 
+                       Card('28', 0),
+                       Card('29', 0)]
 
-Forrest_rules_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\26.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\27.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\28.png'), 0),
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\29.png'), 0)]
+water_rules_cards = [Card('30', 0), 
+                     Card('31', 0), 
+                     Card('32', 0),
+                     Card('33', 0)]
 
-Water_rules_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\30.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\31.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\32.png'), 0),
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\33.png'), 0)]
+house_rules_cards = [Card('34', 0), 
+                     Card('35', 0), 
+                     Card('36', 0),
+                     Card('37', 0)]
 
-House_rules_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\34.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\35.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\36.png'), 0),
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\37.png'), 0)]
+area_rules_cards = [Card('38', 0), 
+                    Card('39', 0), 
+                    Card('40', 0),
+                    Card('41', 0)]
 
-Area_rules_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\38.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\39.png'), 0), 
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\40.png'), 0),
-                        Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\41.png'), 0)]
+rules_cards = [random.choice(forrest_rules_cards), 
+               random.choice(house_rules_cards), 
+               random.choice(water_rules_cards),
+               random.choice(area_rules_cards)]
 
-Rules_cards = [random.choice(Forrest_rules_cards), 
-            random.choice(House_rules_cards), 
-            random.choice(Water_rules_cards),
-            random.choice(Area_rules_cards)]
+monster_cards = [Card('01', 0), 
+                 Card('02', 0), 
+                 Card('03', 0),
+                 Card('04', 0),
+                 Card('A01', 0), 
+                 Card('A02', 0), 
+                 Card('A03', 0),
+                 Card('A04', 0)]
 
-Monster_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\01.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\02.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\03.png'), 0),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\04.png'), 0),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\A01.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\A02.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\A03.png'), 0),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\A04.png'), 0)]
+complete_playing_cards = [Card('05', 0), 
+                 Card('06', 0), 
+                 Card('07', 1),
+                 Card('08', 1),
+                 Card('09', 1), 
+                 Card('10', 1), 
+                 Card('11', 2),
+                 Card('12', 2),
+                 Card('13', 2),
+                 Card('14', 2), 
+                 Card('15', 2), 
+                 Card('16', 2),
+                 Card('17', 0)]
 
-Playing_cards = [Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\05.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\06.png'), 0), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\07.png'), 1),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\08.png'), 1),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\09.png'), 1), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\10.png'), 1), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\11.png'), 2),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\12.png'), 2),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\13.png'), 2),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\14.png'), 2), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\15.png'), 2), 
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\16.png'), 2),
-                Card(pygame.image.load(r'C:\Users\batkova\Downloads\karticky\karticky\17.png'), 0)]
+extension_cards = [Card('S1', 0), 
+                 Card('S2', 0), 
+                 Card('S3', 0),
+                 Card('S4', 0),
+                 Card('S5', 0), 
+                 Card('S6', 0), 
+                 Card('S7', 0),
+                 Card('S8', 0)]
     
 
 
--- game.py
+++ game.py
@@ -2,34 +2,53 @@ import pygame
 import random
 
 import global_def as gd
-import cards as c
+import cards
 
 
 pygame.init()
 
 gd.display.fill(gd.white)
 
-c.draw_letter("A", 200, 450)
-c.draw_letter("B", 400, 450)
-c.draw_letter("C", 600, 450)
-c.draw_letter("D", 800, 450)
+cards.draw_letter("A", 200, 450)
+cards.draw_letter("B", 400, 450)
+cards.draw_letter("C", 600, 450)
+cards.draw_letter("D", 800, 450)
 
-coord = [200,400,600,800]
+coord = [200, 400, 600, 800]
 for i in coord:
-    card = random.choice(c.Rules_cards)
+    card = random.choice(cards.rules_cards)
     card.draw_card(i, 650)
-    c.Rules_cards.remove(card)
+    cards.rules_cards.remove(card)
 
-c.message("Press spacebar for new season", gd.black, 40, 20, 50)
-c.message("Press n for next card", gd.black, 40, 20, 80)
+coord = [400, 600, 800]
+for i in coord:
+    card = random.choice(cards.extension_cards)
+    card.draw_card(i, 110)
+    cards.extension_cards.remove(card)
 
-i = 0
+
+cards.message("Press spacebar for new season", gd.black, 40, 20, 50)
+cards.message("Press n for next card", gd.black, 40, 20, 80)
+
+
+playing_cards = []
+
+monster_cards = []
+for a in range(4):
+    monster_card = random.choice(cards.monster_cards)
+    monster_cards.append(monster_card)
+    cards.monster_cards.remove(monster_card)
+
+played_monster_cards = []
+    
+
+
+
+i = -1
+
 
 def Season(i):
-    c.Seasons[i].draw_card(200, 110)
-    monster = random.choice(c.Monster_cards)
-    c.Playing_cards.append(monster)
-    random.shuffle(c.Playing_cards)
+    pass
 
 
 while True:
@@ -39,12 +58,32 @@ while True:
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    Season(i)
                     i+=1
-                if event.key == pygame.K_n:
-                    random_card = random.choice(c.Playing_cards)
-                    random_card.draw_card(1100, 200)
-                    c.Playing_cards.remove(random_card)
+                    cards.seasons[i].draw_card(200, 110)
+
+                    playing_cards = cards.complete_playing_cards.copy()
+                    for b in range(i+1):
+                        if monster_cards[b] not in played_monster_cards: playing_cards.append(monster_cards[b]) 
+                    random.shuffle(playing_cards)
+
+                    sum = 0
+                    y = 50
+                    pygame.draw.rect(gd.display, gd.white, [1100, 50, 1500, 1500])
+
+                if event.key == pygame.K_n and sum < cards.seasons[i].number:
+                    random_card = random.choice(playing_cards)
+                    random_card.draw_card(1100, y)
+                    playing_cards.remove(random_card)
+                    if random_card in monster_cards:
+                        played_monster_cards.append(random_card)
+                    
+                    y += 70
+                    sum += random_card.number
+                    if sum >= cards.seasons[i].number:
+                        cards.message("Last card!", gd.black, 40, 1140, 940)
+
+
+                
 
     pygame.display.update()
 
